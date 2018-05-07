626dd421c511c45ddcfdb3ec2dc62f51f4939b09
Sarah Grimm <sarahagrimm@gmail.com>
2018-05-07 13:42:39 -0700

    portfolio and php

---
+++ may07/index.php
@@ -0,0 +1,15 @@
+<?php
+    $first = 'Sarah';
+    $last = 'Grimm';
+    $a = 'Sarah';
+    $b = 25;
+    $d = 'purple';
+?>
+
+<?php
+    $x = 5;
+    function test($myVar){
+        echo "The value of x is: " + $myVar;
+    }
+    test($x);
+?>
