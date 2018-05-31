79385954a99e2b316520c976770e496457f34f95
Sarah Grimm <sarahagrimm@gmail.com>
2018-05-31 03:19:45 -0700

    portfolio work

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,3 +1,14 @@
-function changePage() {
-    window.history.pushState("object or string", "Title", "/new-url");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
